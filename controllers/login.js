8e137bd05f305c88e6e634c5008f2fccf67b784a
Ryan Gu <rgu@Ryans-MacBook-Pro.local>
2022-03-03 02:22:01 -0500

    fix5

--- controllers/login.js
+++ controllers/login.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
 const router = require('express').Router()
 const User = require('../models/user')
-const config = require('./utils/config')
+const config = require('../utils/config')
 
 router.post('/', async (request, response) => {
   const body = request.body
